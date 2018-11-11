f584bae3549d4587182fd02b4dd9638f005859e5
MasashiFuruta <33737185+MasashiFuruta@users.noreply.github.com>
2018-11-11 16:18:20 +0900

    add root routing and index page

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  scope module: :web, as: :web do
+    root to: 'root#index'
+  end
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root to: 'home#index'
 end
