2e0ce3b1df76342262bfc859eef2418e443d3cb3
MasashiFuruta <33737185+MasashiFuruta@users.noreply.github.com>
2018-11-25 18:36:12 +0900

    category kari

--- app/controllers/web/categories/bgm_controller.rb
+++ app/controllers/web/categories/bgm_controller.rb
@@ -1,6 +1,7 @@
 class Web::Categories::BgmController < ApplicationController
+  before_action :set_category, only: [:index, :show]
+
   def index
-    @category = "bgm"
     @node = Category.find_by_name(@category)
     render :template => 'web/categories/index'
   end
@@ -8,11 +9,15 @@ class Web::Categories::BgmController < ApplicationController
   def show
     @id = params[:id]
     @children = []
-    node = Category.find(@id)
-    @ancestors = Category.ancestors_of(node)
-    @children = Category.children_of(node)
+    @node = Category.find(@id)
+    @ancestors =  Category.ancestors_of(@node)
+    @children = nil || Category.children_of(@node)
     @root = @ancestors[1]
-    @to_node = @ancestors.to_a.drop(2) << node
+    @to_node =  @ancestors.to_a.drop(2) << @node
     render :template => 'web/categories/show'
+  end
+
+  def set_category
+    @category = "bgm"
   end
 end
